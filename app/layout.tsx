bd62963e5453beace2169084dd23a3130c7e3bf7
Dias <dias_2003_2010@mail.ru>
2025-09-05 23:05:43 +0500

    added theme toggler

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
 import { Toaster } from "sonner";
+import { ThemeProvider } from "@/components/providers/ThemeProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,18 +27,26 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
-        <div className="flex">
-          <div className="hidden md:block h-[100vh] w-[300px]">
-            <Sidebar />
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+          storageKey="dashboard-theme"
+        >
+          <Navbar />
+          <div className="flex">
+            <div className="hidden md:block h-[100vh] w-[300px]">
+              <Sidebar />
+            </div>
+            <div className="p-5 w-full md:max-w-[1140px]">{children}</div>
           </div>
-          <div className="p-5 w-full md:max-w-[1140px]">{children}</div>
-        </div>
-        <Toaster />
+          <Toaster />
+        </ThemeProvider>
       </body>
     </html>
   );
